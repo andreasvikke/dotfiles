1a61a083f75a402a95c71e16e2faa6ad9909fc42
andreasvikke <andreasvikke@gmail.com>
2025-04-30 21:18:33 +0200

    feat: hyprland

---
+++ .home/.config/wal/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wal -i ~/.config/hypr/assets/wallpaper.png --cols16 lighten --saturate 0.4 \
+    -o ~/.config/wal/scripts/chromium.sh \
+    -o ~/.config/wal/scripts/kde.sh
--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
 
-while getopts igm flag; do
+while getopts igmh flag; do
   case "${flag}" in
   i) install=true ;;
   g) gui=true ;;
   m) gnome=true ;;
+  h) hyprland=true ;;
   esac
 done
 
@@ -16,8 +17,7 @@ if [ "$install" ]; then
     sudo pacman-mirrors --geoip && sudo pacman -Syyu --noconfirm
     # Install necessary packages for repositories
     sudo pacman -S --needed --noconfirm - <./.extra/req.pacman
-    echo "Test"
-    # yay -S --needed --noconfirm - <./.extra/req.aur
+    yay -S --needed --noconfirm - <./.extra/req.aur
   elif [ "$distro" == "ubuntu" ]; then
     # Install necessary packages for repositories
     sudo apt update
@@ -51,14 +51,24 @@ if [ "$install" ]; then
     if [ "$gnome" ]; then
       echo "Installing gnome extensions and setting up gnome keybindings"
       ./install-gnome.sh
-
-      echo "Setting GTK theme"
-      GTK='GTK_THEME="Nordic"'
-      if ! grep -Fxq "$GTK" /etc/environment; then
-        echo "$GTK" | sudo tee -a /etc/environment
-      fi
     fi
   fi
+fi
+
+# Install hyprland
+if [ "$hyprland" ]; then
+  echo "Installing Hyperland and dependencies"
+  sudo pacman -S --needed --noconfirm - <./.extra/req.pacman.hypr
+  yay -S --needed --noconfirm - <./.extra/req.aur.hypr
+
+  # Setup PAM hook (login + SDDM)"
+  sudo sed -i \
+    '/^auth[[:space:]]\+include[[:space:]]\+system-auth/ a auth       optional   pam_gnome_keyring.so' \
+    /etc/pam.d/system-login
+
+  sudo sed -i \
+    '/^session[[:space:]]\+include[[:space:]]\+system-auth/ a session    optional   pam_gnome_keyring.so auto_start' \
+    /etc/pam.d/system-login
 fi
 
 # Non distro specific
@@ -80,5 +90,15 @@ fi
 
 # Setup dotfiles
 cp -TR ./.home ~/
+
+if [ "$hyprland" ]; then
+  echo "Setting theme"
+  ~/.config/wal/run.sh
+  mkdir -p ~/.config/Kvantum/pywal
+  ln -sf ~/.cache/wal/pywal.kvconfig ~/.config/Kvantum/pywal/pywal.kvconfig
+  ln -sf ~/.cache/wal/pywal.svg ~/.config/Kvantum/pywal/pywal.svg
+  ls -sf ~/.cache/wal/pywal.json ~/.config/presets/user/pywal.json
+  sudo chmod a+wr /opt/spotify && sudo chmod a+wr /opt/spotify/Apps -R
+fi
 
 echo "Install Script Complete!"
